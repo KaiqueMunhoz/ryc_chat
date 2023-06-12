2c2187cfcd00c6df42457705b1a0596823e24e43
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-12 17:44:33 -0300

    feat: Use AuthPage as home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ryc_chat/pages/loading_page.dart';
+import 'package:ryc_chat/pages/auth_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoadingPage(),
+      home: const AuthPage(),
       debugShowCheckedModeBanner: false,
     );
   }
