2ff1f0abcabe671d7b1993a8f8a1236c943d2472
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-12 17:44:08 -0300

    feat: Add Scaffold and use AuthForm

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:ryc_chat/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: AuthForm(),
+        ),
+      ),
+    );
   }
 }
