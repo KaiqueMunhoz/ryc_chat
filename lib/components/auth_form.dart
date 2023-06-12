96be764e38ca3d436f8d392345371ec9d199bfd6
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-12 18:24:48 -0300

    feat: Convert AuthForm to StatefulWidget

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
   const AuthForm({Key? key}) : super(key: key);
 
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
   @override
   Widget build(BuildContext context) {
     return Card(
