e1ea2a4ee18bf85f37775256016edceedd687f17
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-12 17:48:53 -0300

    feat: Wrap Form with Padding all 16

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -7,20 +7,23 @@ class AuthForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(20),
-      child: Form(
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Nome'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'E-mail'),
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Senha'),
-            ),
-          ],
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Nome'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'E-mail'),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(labelText: 'Senha'),
+              ),
+            ],
+          ),
         ),
       ),
     );
