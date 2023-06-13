e371cba6e35976088d1758399d359cb25b6131e7
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-13 06:59:54 -0300

    feat: Show a CircularProgressIndicator when isLoading is true

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -29,6 +29,15 @@ class _AuthPageState extends State<AuthPage> {
               ),
             ),
           ),
+          if (_isLoading)
+            Container(
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
         ],
       ),
     );
