7f3725e3ada6c6202bb594d4ec674b302f14ae44
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-13 08:06:54 -0300

    feat: Create a draft of AuthMockService

---
+++ lib/core/services/auth/auth_mock_service.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:ryc_chat/core/models/chat_user.dart';
+import 'package:ryc_chat/core/services/auth/auth_service.dart';
+
+class AuthMockService implements AuthService {
+  @override
+  // TODO: implement currentUser
+  ChatUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> login({required String email, required String password}) {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signup(
+      {required String nome,
+      required String email,
+      required String password,
+      required File image}) {
+    // TODO: implement signup
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement userChanges
+  Stream<ChatUser?> get userChanges => throw UnimplementedError();
+}
