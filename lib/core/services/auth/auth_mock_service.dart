7ee9ceabc0666a395db9af23a69898ec0be8b3a7
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-13 08:20:23 -0300

    feat: Create MultiStreamController<ChatUser>? _controller

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:ryc_chat/core/models/chat_user.dart';
@@ -6,6 +7,7 @@ import 'package:ryc_chat/core/services/auth/auth_service.dart';
 class AuthMockService implements AuthService {
   static Map<String, ChatUser> _users = {};
   static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser>? _controller;
 
   @override
   // TODO: implement currentUser
