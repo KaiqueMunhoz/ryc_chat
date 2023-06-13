02df31ef4c24e2b67b322afc49ef0b408dd869e5
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-13 08:12:26 -0300

    feat: Create _users and _currentUser

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -4,6 +4,9 @@ import 'package:ryc_chat/core/models/chat_user.dart';
 import 'package:ryc_chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+
   @override
   // TODO: implement currentUser
   ChatUser? get currentUser => throw UnimplementedError();
