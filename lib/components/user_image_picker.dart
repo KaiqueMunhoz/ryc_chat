c2f965dac1b53c6c6e9e951cdd29bcb5cafacdf7
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-13 07:33:49 -0300

    feat: Create method _pickImage

--- lib/components/user_image_picker.dart
+++ lib/components/user_image_picker.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
   final void Function(File image) onImagePick;
@@ -16,6 +17,23 @@ class UserImagePicker extends StatefulWidget {
 
 class _UserImagePickerState extends State<UserImagePicker> {
   File? _image;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+
+      widget.onImagePick(_image!);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
