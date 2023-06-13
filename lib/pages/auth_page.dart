7433a5203078f8c2703401aaecf9331482f255d1
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-13 07:49:10 -0300

    feat: Change _handleSubmit to be a future, add try/catch/finally

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -12,8 +12,12 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   bool _isLoading = false;
 
-  void _handleSubmit(AuthFormData formData) {
-    setState(() => _isLoading = true);
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isLoading = true);
+    } catch (error) {} finally {
+      setState(() => _isLoading = false);
+    }
   }
 
   @override
