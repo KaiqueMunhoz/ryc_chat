a0c525834b13c94da8ffd065e10a5c2fe7a648a1
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-13 07:12:26 -0300

    feat: Add CircleAvatar

--- lib/components/user_image_picker.dart
+++ lib/components/user_image_picker.dart
@@ -5,6 +5,13 @@ class UserImagePicker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: const [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+        )
+      ],
+    );
   }
 }
