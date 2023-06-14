7e6ef99cfbab4bb226b95fc0b658f7a90b4c2ca7
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 18:40:42 -0300

    feat: Add logout button

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:ryc_chat/core/services/auth/auth_mock_service.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              child: Text('Logout'),
+              onPressed: () {
+                AuthMockService().logout();
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
