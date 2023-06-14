d4bc36dcea4716d95d8acd98ad67fd9581c65f67
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 19:40:49 -0300

    feat: Add messagesStream() method to ChatService

--- lib/core/services/chat/chat_service.dart
+++ lib/core/services/chat/chat_service.dart
@@ -2,6 +2,8 @@ import 'package:ryc_chat/core/models/chat_message.dart';
 import 'package:ryc_chat/core/models/chat_user.dart';
 
 abstract class ChatService {
+  Stream<List<ChatMessage>> messagesStream();
+
   Future<ChatMessage> save({
     required String text,
     required ChatUser user,
