58ca3d82531ef602389c18816247ae278fa116ac
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 20:21:05 -0300

    feat: Create a draft of MessagesBubble component to lib/components
    This commit adds a new file, messages_bubble.dart, which contains the MessagesBubble component. This component is a stateless widget.

---
+++ lib/components/messages_bubble.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MessagesBubble extends StatelessWidget {
+  const MessagesBubble({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
