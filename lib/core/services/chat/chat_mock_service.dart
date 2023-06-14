6a71a71948388d39be61d947550a80168be3010d
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 19:43:25 -0300

    feat: Create ChatMockService class thats implement ChatService interface.

---
+++ lib/core/services/chat/chat_mock_service.dart
@@ -0,0 +1,15 @@
+import 'package:ryc_chat/core/models/chat_user.dart';
+import 'package:ryc_chat/core/models/chat_message.dart';
+import 'package:ryc_chat/core/services/chat/chat_service.dart';
+
+class ChatMockService implements ChatService {
+  @override
+  Stream<List<ChatMessage>> messagesStream() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ChatMessage> save({required String text, required ChatUser user}) {
+    throw UnimplementedError();
+  }
+}
