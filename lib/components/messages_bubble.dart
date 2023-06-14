d80e291e1ff25350cf1436ca6026da2beb51f1d9
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 20:22:13 -0300

    feat: Add ChatMessage model to MessagesBubble
    The code changes add the ChatMessage model to the MessagesBubble component, allowing for more dynamic and customizable chat messages.

--- lib/components/messages_bubble.dart
+++ lib/components/messages_bubble.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:ryc_chat/core/models/chat_message.dart';
 
 class MessagesBubble extends StatelessWidget {
-  const MessagesBubble({Key? key}) : super(key: key);
+  final ChatMessage message;
+
+  const MessagesBubble({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
