9f4386f0dfba794a9320077c81d6d6171e3ed354
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 20:12:36 -0300

    feat: Add ChatService to Messages widget

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:ryc_chat/core/services/chat/chat_mock_service.dart';
+import 'package:ryc_chat/core/services/chat/chat_service.dart';
 
 class Messages extends StatelessWidget {
   const Messages({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder(
+      stream: ChatService().messagesStream(),
+      builder: (_, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Container();
+      },
+    );
   }
 }
