2d33eba836426bef361c410947e657bba8353bfc
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 20:02:21 -0300

    feat: Add mock chat messages to ChatMockService

--- lib/core/services/chat/chat_mock_service.dart
+++ lib/core/services/chat/chat_mock_service.dart
@@ -6,10 +6,36 @@ import 'package:ryc_chat/core/models/chat_message.dart';
 import 'package:ryc_chat/core/services/chat/chat_service.dart';
 
 class ChatMockService implements ChatService {
-  static final List<ChatMessage> _msgs = [];
+  static final List<ChatMessage> _msgs = [
+    ChatMessage(
+      id: '1',
+      text: 'Bom dia',
+      createdAt: DateTime.now(),
+      userId: '123',
+      userName: 'Bia',
+      userImageURL: '',
+    ),
+    ChatMessage(
+      id: '2',
+      text: 'Bom dia. Teremos reunião hoje?',
+      createdAt: DateTime.now(),
+      userId: '123',
+      userName: 'Ana',
+      userImageURL: '',
+    ),
+    ChatMessage(
+      id: '1',
+      text: 'Sim. Pode ser agora?',
+      createdAt: DateTime.now(),
+      userId: '123',
+      userName: 'Bia',
+      userImageURL: '',
+    ),
+  ];
   static MultiStreamController<List<ChatMessage>>? _controller;
   static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
     _controller = controller;
+    controller.add(_msgs);
   });
 
   @override
