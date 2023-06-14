c100df4a3a52e727dea3b6b229599af371363f93
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 19:55:13 -0300

    feat: Add messagesStream to ChatMockService
    This commit adds a new feature to the ChatMockService by implementing the messagesStream method. The method returns a stream of chat messages and uses a MultiStreamController to manage subscriptions.

--- lib/core/services/chat/chat_mock_service.dart
+++ lib/core/services/chat/chat_mock_service.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
+
 import 'package:ryc_chat/core/models/chat_user.dart';
 import 'package:ryc_chat/core/models/chat_message.dart';
 import 'package:ryc_chat/core/services/chat/chat_service.dart';
 
 class ChatMockService implements ChatService {
   static final List<ChatMessage> _msgs = [];
+  static MultiStreamController<List<ChatMessage>>? _controller;
+  static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
+    _controller = controller;
+  });
+
   @override
   Stream<List<ChatMessage>> messagesStream() {
-    throw UnimplementedError();
+    return _msgsStream;
   }
 
   @override
