f6fe2d80e3037b8470f35863d772ffa73f17bec8
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 18:59:03 -0300

    feat: Create and use _defaultUser

--- lib/core/services/auth/auth_mock_service.dart
+++ lib/core/services/auth/auth_mock_service.dart
@@ -6,12 +6,19 @@ import 'package:ryc_chat/core/models/chat_user.dart';
 import 'package:ryc_chat/core/services/auth/auth_service.dart';
 
 class AuthMockService implements AuthService {
+  static final _defaultUser = ChatUser(
+    id: '1',
+    name: 'Teste',
+    email: 'teste@',
+    imageURL: 'assets/images/avatar.png',
+  );
+
   static Map<String, ChatUser> _users = {};
   static ChatUser? _currentUser;
   static MultiStreamController<ChatUser?>? _controller;
   static final _userStream = Stream<ChatUser?>.multi((controller) {
     _controller = controller;
-    _updateUser(null);
+    _updateUser(_defaultUser);
   });
 
   @override
