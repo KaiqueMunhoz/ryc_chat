23fdb46c57ff748849d905fe4f00639f0801f70f
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 19:52:42 -0300

    feat: Add empty list of chat messages to ChatMockService

--- lib/core/services/chat/chat_mock_service.dart
+++ lib/core/services/chat/chat_mock_service.dart
@@ -3,6 +3,7 @@ import 'package:ryc_chat/core/models/chat_message.dart';
 import 'package:ryc_chat/core/services/chat/chat_service.dart';
 
 class ChatMockService implements ChatService {
+  static final List<ChatMessage> _msgs = [];
   @override
   Stream<List<ChatMessage>> messagesStream() {
     throw UnimplementedError();
