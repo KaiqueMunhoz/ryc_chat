b1e26d44fc758f364aa69a090dd770d494d866c5
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 18:25:44 -0300

    feat: Add StreamBuilder to AuthOrAppPage

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:ryc_chat/core/services/auth/auth_mock_service.dart';
+import 'package:ryc_chat/pages/auth_page.dart';
+import 'package:ryc_chat/pages/chat_page.dart';
+import 'package:ryc_chat/pages/loading_page.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: StreamBuilder(
+        stream: AuthMockService().userChanges,
+        builder: (_, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return LoadingPage();
+          }
+          return snapshot.hasData ? ChatPage() : AuthPage();
+        },
+      ),
+    );
   }
 }
