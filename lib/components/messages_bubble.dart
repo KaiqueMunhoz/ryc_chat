815879940347505191fe9e868e063a1b50afc8c0
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-14 21:20:16 -0300

    feat: Refactor MessagesBubble widget to include circle avatar
    This commit refactors the MessagesBubble widget to include a user avatar. The avatar is positioned at the top of the message bubble and is represented by a red circle for now. This change improves the visual representation of messages in the chat app.

--- lib/components/messages_bubble.dart
+++ lib/components/messages_bubble.dart
@@ -13,52 +13,64 @@ class MessagesBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: belongsToCurrentUser
-          ? MainAxisAlignment.end
-          : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: belongsToCurrentUser
-                ? Colors.grey.shade300
-                : Theme.of(context).accentColor,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12),
-              topRight: Radius.circular(12),
-              bottomLeft: belongsToCurrentUser
-                  ? Radius.circular(12)
-                  : Radius.circular(0),
-              bottomRight: belongsToCurrentUser
-                  ? Radius.circular(0)
-                  : Radius.circular(12),
+        Row(
+          mainAxisAlignment: belongsToCurrentUser
+              ? MainAxisAlignment.end
+              : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: belongsToCurrentUser
+                    ? Colors.grey.shade300
+                    : Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: belongsToCurrentUser
+                      ? Radius.circular(12)
+                      : Radius.circular(0),
+                  bottomRight: belongsToCurrentUser
+                      ? Radius.circular(0)
+                      : Radius.circular(12),
+                ),
+              ),
+              width: 180,
+              margin: const EdgeInsets.symmetric(
+                vertical: 15,
+                horizontal: 8,
+              ),
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    message.userName,
+                    style: TextStyle(
+                      color: belongsToCurrentUser ? Colors.black : Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    message.text,
+                    style: TextStyle(
+                      color: belongsToCurrentUser ? Colors.black : Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          width: 180,
-          margin: const EdgeInsets.symmetric(
-            vertical: 15,
-            horizontal: 8,
-          ),
-          padding: const EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 16,
-          ),
-          child: Column(
-            children: [
-              Text(
-                message.userName,
-                style: TextStyle(
-                  color: belongsToCurrentUser ? Colors.black : Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                message.text,
-                style: TextStyle(
-                  color: belongsToCurrentUser ? Colors.black : Colors.white,
-                ),
-              ),
-            ],
+          ],
+        ),
+        Positioned(
+          top: 0,
+          left: belongsToCurrentUser ? null : 165,
+          right: belongsToCurrentUser ? 165 : null,
+          child: CircleAvatar(
+            backgroundColor: Colors.red,
           ),
         ),
       ],
