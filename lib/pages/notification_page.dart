f8214d8259c585acb37a74b950d5b5b3e515a981
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-15 08:18:28 -0300

    feat: Add provider to NotificationPage and use ListView.builder

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ryc_chat/core/services/notification/chat_notification_service.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final service = Provider.of<ChatNotificationService>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Minhas Notificações'),
+      ),
+      body: ListView.builder(
+        itemCount: service.itemsCount,
+        itemBuilder: (_, index) {
+          return Container();
+        },
       ),
     );
   }
