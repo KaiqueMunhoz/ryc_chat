53e5cb0d618289abeeb779b752c712ab1cde6daa
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-15 07:47:01 -0300

    feat: Create a getter for items

--- lib/core/services/notification/push_notification_service.dart
+++ lib/core/services/notification/push_notification_service.dart
@@ -4,6 +4,10 @@ import 'package:ryc_chat/core/models/chat_notification.dart';
 class PushNotificationService with ChangeNotifier {
   List<ChatNotification> _items = [];
 
+  List<ChatNotification> get items {
+    return [..._items];
+  }
+
   void add(ChatNotification notification) {
     _items.add(notification);
     notifyListeners();
