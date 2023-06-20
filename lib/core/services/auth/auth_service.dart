9299f60ccdd49ff72086ad439456b4572c6fb146
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-20 00:27:36 -0300

    feat: Use AuthFirebaseService

--- lib/core/services/auth/auth_service.dart
+++ lib/core/services/auth/auth_service.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:ryc_chat/core/models/chat_user.dart';
 import 'package:ryc_chat/core/services/auth/auth_mock_service.dart';
 
+import 'auth_firebase_service.dart';
+
 abstract class AuthService {
   ChatUser? get currentUser;
 
@@ -23,6 +25,6 @@ abstract class AuthService {
   Future<void> logout();
 
   factory AuthService() {
-    return AuthMockService();
+    return AuthFirebaseService();
   }
 }
