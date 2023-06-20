bf781038d4f863f542f1a1e7b668e6885cba199f
Kaique Munhoz <munhoz13km@gmail.com>
2023-06-19 21:38:06 -0300

    feat: Create a draft of AuthFirebaseService

---
+++ lib/core/services/auth/auth_firebase_service.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:ryc_chat/core/models/chat_user.dart';
+import 'package:ryc_chat/core/services/auth/auth_service.dart';
+
+class AuthFirebaseService implements AuthService {
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+  });
+
+  @override
+  ChatUser? get currentUser {
+    return _currentUser;
+  }
+
+  @override
+  Future<void> login({required String email, required String password}) async {}
+
+  @override
+  Future<void> logout() async {}
+
+  @override
+  Future<void> signup({
+    required String name,
+    required String email,
+    required String password,
+    File? image,
+  }) async {}
+
+  @override
+  Stream<ChatUser?> get userChanges {
+    return _userStream;
+  }
+}
